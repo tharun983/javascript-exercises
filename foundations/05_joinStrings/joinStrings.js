c880523eabff881211de9f577a9a9eb482b8c3c2
Tharun <pappulat41@gmail.com>
2026-05-25 17:02:52 +0530

    05 exercise

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -8,6 +8,14 @@
 */
 
 // Add your code right below, good luck!
+let firstName = "Carlos";
+let lastName = 'Stevenson';
+let thisYear = 1965;
+let birthYear = 1947;
+
+let greeting = "Hello! My name is " + firstName + " " + lastName + " and I am 18 years old.";
+
+console.log(greeting);
 
 
 
