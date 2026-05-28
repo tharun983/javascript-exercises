def3cbc94dd20e70926388c89351bfe55e45de61
Tharun <pappulat41@gmail.com>
2026-05-28 12:10:09 +0530

    06 exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,7 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    return string.repeat(num);
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
